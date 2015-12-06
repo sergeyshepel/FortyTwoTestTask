fe1518de36553e22e5114c4c9016c55e54566fe0
Sergey Shepel <sergey.shepel@outlook.com>
2015-12-06 21:39:23 +0000

    ticket:8 Fixed failing tests

--- apps/hello/signals/signals.py
+++ apps/hello/signals/signals.py
@@ -6,19 +6,16 @@ from hello.models import DBActionsLog
 
 @receiver(post_save)
 def object_created_or_updated_signal(sender, created, **kwargs):
-    if sender.__name__ != 'DBActionsLog':
-        if created:
-            DBActionsLog.objects.create(model=sender.__name__,
-                                        action='created')
-        else:
-            DBActionsLog.objects.create(model=sender.__name__,
-                                        action='updated')
-    return
+    if sender.__name__ == 'DBActionsLog':
+        return
+
+    action = 'created' if created else 'updated'
+    DBActionsLog.objects.create(model=sender.__name__, action=action)
 
 
 @receiver(post_delete)
 def object_removed_signal(sender, **kwargs):
-    if sender.__name__ != 'DBActionsLog':
-        DBActionsLog.objects.create(model=sender.__name__,
-                                    action='deleted')
-    return
+    if sender.__name__ == 'DBActionsLog':
+        return
+
+    DBActionsLog.objects.create(model=sender.__name__, action='deleted')
