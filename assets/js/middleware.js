afdddcffe58dc231b8572ac1c9a0796312d0442a
Sergey Shepel <sergey.shepel@outlook.com>
2015-12-06 20:54:07 +0000

    ticket:13 Add sorting for priority field

--- assets/js/middleware.js
+++ assets/js/middleware.js
@@ -2,49 +2,91 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+function sortTable(table_body, order) {
+    table_body.find('tr').sort(function(a,b){
+        var tda = $(a).find('td:eq(1)').text();
+        var tdb = $(b).find('td:eq(1)').text();
+        if (order === 0) {
+            return tda > tdb ? 1
+                   : tda < tdb ? -1
+                   : 0;
+        } else {
+            return tda < tdb ? 1
+                   : tda > tdb ? -1
+                   : 0;
+        }
+    }).appendTo(table_body);
+}
+
 $( function() {
-  setInterval( function() {
-    var latest_id = $( ".table tbody>tr>td:first-child" ).html();
+    // asc
+    $( "#requests_table" ).on( "click", "th.headerSortDown", function(){
+        $(this).toggleClass("headerSortDown headerSortUp");
+        sortTable($("#requests_table tbody"), 0);
+    });
+    // desc
+    $( "#requests_table" ).on( "click", "th.headerSortUp", function(){
+        $(this).toggleClass("headerSortUp headerSortDown");
+        sortTable($("#requests_table tbody"), 1);
+    });
+    // ajax
+    setInterval( function() {
+        var ids=[];
 
-    $.getJSON( "/requests/", { "latest_id": latest_id.toString() }, function( data ) {
-        var default_title = $("title").text();
-        var new_requests = jQuery.parseJSON(data.requests);
-        var number_of_new_requests = Object.keys(new_requests).length;
-        var current_number_of_rows = $(".table tbody").children().length;
+        // iterate all td's in hidden id column
+        $('#requests_table tbody tr td:nth-child(1)').each( function(){
+            //add item to array
+            ids.push( $(this).text() );
+        });
 
-        function unset_title( title ) {
-            $( "title" ).text( title );
-        };
+        var latest_id = Math.max.apply(Math, ids);
 
-        function prepend_new_requests(response_data, requests_in_response, rows_on_page){
-            $( "title" ).text('(' + requests_in_response + ')' + " new requests stored in db!");
+        $.getJSON( "/requests/", { "latest_id": latest_id.toString() }, function( data ) {
+            var default_title = $("title").text();
+            var new_requests = jQuery.parseJSON(data.requests);
+            var number_of_new_requests = Object.keys(new_requests).length;
+            var current_number_of_rows = $(".table tbody").children().length;
 
-            $.each(response_data, function( index, item ){
-                var isoDate  = new Date(item.fields.time).toUTCString().replace("GMT", "");
-                tr = '<tr><td hidden>'+ item.pk+'</td>';
-                tr += '<td>' + item.fields.priority+'</td>';
-                tr += '<td>' + isoDate+'</td>';
-                tr += '<td>' + item.fields.path+'</td>';
-                tr += '<td>' + item.fields.method+'</td>';
-                tr += '<td>' + item.fields.user_agent+'</td>';
-                tr += '<td>' + item.fields.remote_addr+'</td>';
-                tr += '<td>' + item.fields.is_secure.toString().capitalize()+'</td>';
-                tr += '<td>' + item.fields.is_ajax.toString().capitalize()+'</td>';
-                tr += '</tr>';
+            function unset_title( title ) {
+                $( "title" ).text( title );
+            };
 
-                $(".table  > tbody > tr:first").before(tr);
-            });
+            function prepend_new_requests(response_data, requests_in_response, rows_on_page){
+                $( "title" ).text('(' + requests_in_response + ')' + " new requests stored in db!");
 
-            if (rows_on_page + requests_in_response > 10) {
-                for(var i = 0; i < (requests_in_response); i++) {
-                    $(".table tbody tr:last-child").remove();
+                $.each(response_data, function( index, item ){
+                    var isoDate  = new Date(item.fields.time).toUTCString().replace("GMT", "");
+                    tr = '<tr><td hidden>'+ item.pk+'</td>';
+                    tr += '<td>' + item.fields.priority+'</td>';
+                    tr += '<td>' + isoDate+'</td>';
+                    tr += '<td>' + item.fields.path+'</td>';
+                    tr += '<td>' + item.fields.method+'</td>';
+                    tr += '<td>' + item.fields.user_agent+'</td>';
+                    tr += '<td>' + item.fields.remote_addr+'</td>';
+                    tr += '<td>' + item.fields.is_secure.toString().capitalize()+'</td>';
+                    tr += '<td>' + item.fields.is_ajax.toString().capitalize()+'</td>';
+                    tr += '</tr>';
+
+                    $(".table  > tbody > tr:first").before(tr);
+                });
+
+                if (rows_on_page + requests_in_response > 10) {
+                    for(var i = 0; i < (requests_in_response); i++) {
+                        $(".table tbody tr:last-child").remove();
+                    }
                 }
             }
-        }
-        if (number_of_new_requests > 0) {
-            prepend_new_requests(new_requests, number_of_new_requests, current_number_of_rows);
-            setTimeout(unset_title, 5500, default_title);
-        }
-    });
-  }, 7000);
+            if (number_of_new_requests > 0) {
+                prepend_new_requests(new_requests, number_of_new_requests, current_number_of_rows);
+                // sort
+                var priorityClassName = $('#requests_table th:nth-child(2)').attr('class');
+                if (priorityClassName === 'headerSortDown'){
+                    sortTable($("#requests_table tbody"), 1);
+                } else if (priorityClassName === 'headerSortUp') {
+                    sortTable($("#requests_table tbody"), 0);
+                }
+                setTimeout(unset_title, 5500, default_title);
+            }
+        });
+    }, 7000);
 });
